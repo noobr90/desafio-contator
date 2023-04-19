ccd3b8e0311e3851fbef96c8e18fe1f788f92957
Bizantino <noobr90@yahoo.com.br>
2023-04-18 21:21:09 -0300

    exercicio pronto

--- Contador/src/Contador.java
+++ Contador/src/Contador.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
 public class Contador {
-    public static void main(String[] args) throws Exception {
-        System.out.println(" ola ");
-    }
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = sc.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = sc.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+			
+			
+		}catch (ParametrosInvalidosException e) {
+			System.out.println(e.getMessage());
+		}
+		}
+		
+	
+		static void contar(int parametroUm,int parametroDois)throws ParametrosInvalidosException{
+			if(parametroUm>parametroDois){
+				throw new ParametrosInvalidosException("O segundo parametro deve ser maior que o primeiro");
+			}else{
+			int i;
+			int cont = parametroDois - parametroUm;
+			for (i=0;i<cont;i++){
+				System.out.println(parametroUm);
+				System.out.println(parametroDois);
+				}
+			}
+		}
 }
+
